3c9893e1ea0349502d7bc19573a371f5d6c80a1a
sohan-99 <sohan75632@gmail.com>
2023-10-04 02:19:10 +0600

    lefy an d right side work almost done

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,27 @@
-// import React from 'react';
+/* eslint-disable react/jsx-no-undef */
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+    
+    useEffect(() => {
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+        .catch(error => {
+            console.error('Error fetching categories:', error);
+        });
+    }, []);
+
     return (
-        <div>
-            <h2 className="text-3xl">LeftSideNav</h2>
+        <div className="space-y-6">
+            <h2 className="text-3xl">All Categories</h2>
+            {
+                categories.map(category => (
+                    <Link className="block ml-4 text-xl font-semibold" key={category.id} to={`/category/${category.id}`}>{category.name}</Link>
+                ))
+            }
         </div>
     );
 };
--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,50 @@
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable no-unused-vars */
 // import React from 'react';
-
+import qzone1 from '../../../assets/qZone1.png'
+import qzone2 from '../../../assets/qZone2.png'
+import qzone3 from '../../../assets/qZone3.png'
+import { FcGoogle } from 'react-icons/Fc';
+import { FaFacebook } from 'react-icons/Fa';
+import { AiOutlineGithub,AiFillTwitterCircle,AiOutlineInstagram } from 'react-icons/Ai';
 const RightSideNav = () => {
     return (
         <div>
-            <h2 className="text-3xl">RightSideNav</h2>
+{/* login  */}
+            <div className='p-4 space-y-3 mb-6'>
+                <h2 className="text-3xl">Login With</h2>
+                <button className="btn btn-outline w-full">
+                    <FcGoogle></FcGoogle>
+                    Login With Google
+                </button>
+                <button className="btn btn-outline w-full">
+                    <AiOutlineGithub></AiOutlineGithub>
+                    Login With GitHub
+                </button>
+            </div>
+{/* find us  */}
+            <div className='p-4  mb-6'>
+                <h2 className="text-3xl mb-4">Find us</h2>
+                <a className='items-center flex p-4 border rounded-t-lg' href="">
+                    <FaFacebook className='mr-3'></FaFacebook>
+                    Facbook
+                </a>
+                <a className='items-center flex p-4 border-x ' href="">
+                    <AiOutlineInstagram className='mr-3'></AiOutlineInstagram>
+                    Twitter
+                </a>
+                <a className='items-center flex p-4 border rounded-b-lg' href="">
+                    <AiFillTwitterCircle className='mr-3'></AiFillTwitterCircle>
+                    Twitter
+                </a>
+            </div>
+{/* q-zone  */}
+<div className='p-4 space-y-3 mb-6'>
+                <h2 className="text-3xl">Q-Zone</h2>
+                <img src={qzone1} alt="" />
+                <img src={qzone2} alt="" />
+                <img src={qzone3} alt="" />
+            </div>
         </div>
     );
 };
